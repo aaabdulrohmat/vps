97637945e040362bb3e3292e1b7e47a6639da73d
aaabdulrohmat <75303592+aaabdulrohmat@users.noreply.github.com>
2021-06-28 09:21:46 +0700

    Add files via upload

---
+++ miner-microbitcoin.bat
@@ -0,0 +1,10 @@
+rem
+rem Choose nearest stratum:
+rem       stratum-ru.rplant.xyz   /Moscow/
+rem       stratum-eu.rplant.xyz   /London/
+rem       stratum-asia.rplant.xyz /Singapore/
+rem       stratum-na.rplant.xyz   /Toronto/
+rem
+:start
+"%~dp0"cpuminer-sse2.exe -a power2b -o stratum-asia.rplant.xyz:17022 -u MnJoctqbc5rdEXAroxSCPFNfKGmj4HxQoQ.aa1
+goto start
